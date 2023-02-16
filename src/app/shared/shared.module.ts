dffb52e85459cfb0ae8e432350c3784cbd816fe4
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-02-16 07:29:50 +0100

    Added reactive forms.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,24 +6,26 @@ import { TabsContainerComponent } from './tabs-container/tabs-container.componen
 import { TabComponent } from './tab/tab.component';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
     TabComponent,
-    InputComponent
+    InputComponent,
+    AlertComponent
   ],
   imports: [
     CommonModule,
-		ReactiveFormsModule
+		ReactiveFormsModule,
   ],
 	exports: [
 		ModalComponent,
 		TabsContainerComponent,
 		TabComponent,
-		InputComponent
+		InputComponent,
+		AlertComponent
 	],
 	providers: [
 		ModalService
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -11,11 +11,28 @@ export class RegisterComponent {
 		Validators.required,
 		Validators.minLength(3)
 	])
-	email = new FormControl('')
-	age = new FormControl('')
-	password = new FormControl('')
-	confirm_password = new FormControl('')
-	phoneNumber = new FormControl('')
+	email = new FormControl('', [
+		Validators.required,
+		Validators.email,
+		Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
+	)
+	age = new FormControl('', [
+		Validators.required,
+		Validators.min(18),
+		Validators.max(120)
+	])
+	password = new FormControl('', [
+		Validators.required,
+		Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')
+	])
+	confirm_password = new FormControl('', [
+		Validators.required,
+	])
+	phoneNumber = new FormControl('', [
+		Validators.required,
+		Validators.minLength(10),
+		Validators.maxLength(13)
+	])
 
 	registerForm = new FormGroup({
 		name: this.name,
@@ -26,8 +43,17 @@ export class RegisterComponent {
 		phoneNumber: this.phoneNumber
 	});
 
+	showAlert = false;
+	alertMsg = 'Please wait! Your account is eing created.';
+	alertColor = 'blue';
+
 	
 	constructor() {
 	}
 
+	register() {
+		this.showAlert = true;
+		this.alertMsg = 'Please wait! Your account is eing created.';
+		this.alertColor = 'blue';
+	}
 }
