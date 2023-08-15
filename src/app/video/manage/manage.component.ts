1a6010c375d033005ddc7c84d9022373ae5417f5
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-08-15 08:06:47 +0200

    Added query within url for video id.

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./manage.component.scss']
 })
 export class ManageComponent {
+	videoOrder = '1';
+
+	constructor(private router: Router, private route: ActivatedRoute) { }
+
+	ngOnInit() : void {
+		this.router.events.subscribe((event: Params) => {
+			if (event instanceof PopStateEvent) {
+				this.videoOrder = this.route.snapshot.queryParamMap.get('sort') || 'name';
+			}
+		});
+	}
+
+	sort(event: Event) {
+		const { value } = event.target as HTMLSelectElement;
+
+		this.router.navigateByUrl(`/manage?sort=${value}`);
+	}
 
 }
