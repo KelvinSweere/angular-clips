cf7eb33d93a1d8d162ab166dffd56fe0ac68e79c
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-08-17 08:16:26 +0200

    Added guard that will check if user is logged-in.

--- src/app/video/video-routing.module.ts
+++ src/app/video/video-routing.module.ts
@@ -3,16 +3,31 @@ import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from '../upload/upload.component';
 
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['/']);
+
 const routes: Routes = [
 	{ 
 		path: 'manage', 
 		component: ManageComponent,
-		 data: { authOnly: true } 
+		 data: { 
+			authOnly: true,
+			authGuardPipe: redirectUnauthorizedToLogin
+		},
+		canActivate: [AngularFireAuthGuard]
 	}, 
 	{
 		path: 'upload',
 		component: UploadComponent,
-		data: { authOnly: true }
+		data: { 
+			authOnly: true,
+			authGuardPipe: redirectUnauthorizedToLogin
+		},
+		canActivate: [AngularFireAuthGuard]
+	}, {
+		path: 'manage-clips',
+		redirectTo: 'manage',
 	}
 ];
 
