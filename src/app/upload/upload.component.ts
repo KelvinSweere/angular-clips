2835450c1ef83d298d20b6e00124b274463d12ae
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-08-18 10:36:17 +0200

    Added template form for uploading the form.

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./upload.component.scss']
 })
 export class UploadComponent {
+	isDragOver = false;
+	file: File | null = null;
+	nextStep = false;
 
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+	handleDragOver(event: DragEvent) {
+		event.preventDefault();
+		this.isDragOver = true;
+}
+
+	storeFile($event: DragEvent) {
+		$event.preventDefault();
+		this.isDragOver = false;
+
+		this.file = $event.dataTransfer?.files[0] || null;
+
+		if(!this.file || this.file.type !== 'video/mp4') {
+			this.file = null;
+			alert('Only video files are allowed');
+			return;
+		}
+
+		this.nextStep = true;
+
+
+
+	}
 }
