a1c6b5faaf3668073190ab8ab0513404c718d3c0
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-08-28 09:19:35 +0200

    Added input form to edit component.

--- src/app/video/edit/edit.component.ts
+++ src/app/video/edit/edit.component.ts
@@ -1,14 +1,33 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, OnChanges } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 import IClip from 'src/app/models/clip.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
   styleUrls: ['./edit.component.scss']
 })
-export class EditComponent implements OnDestroy, OnInit {
+export class EditComponent implements OnDestroy, OnInit, OnChanges {
 	@Input() clip: IClip | null = null;
+
+	clipID = new FormControl('', {
+		nonNullable: true
+	});
+
+
+	title = new FormControl('', {
+		validators: [
+			Validators.required,
+			Validators.minLength(3),
+		],
+		nonNullable: true
+	});
+	
+	editForm = new FormGroup({
+		title: this.title,
+		clipID: this.clipID
+	});
 
 	constructor(
 		private modal: ModalService
@@ -20,6 +39,14 @@ export class EditComponent implements OnDestroy, OnInit {
 
 	ngOnDestroy(): void {
 		this.modal.unregister('editClip');
+	}
+
+	ngOnChanges(): void {
+		if (!this.clip) {
+			return;
+		}
+		this.clipID.setValue(this.clip.docID!);
+		this.title.setValue(this.clip.title);
 	}
 
 }
