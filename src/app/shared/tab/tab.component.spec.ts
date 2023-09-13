93624d65b0c8ef042155f5607a99de63dadba1bf
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-09-13 15:37:40 +0200

    Added test for 1 tab.

--- src/app/shared/tab/tab.component.spec.ts
+++ src/app/shared/tab/tab.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TabComponent } from './tab.component';
+import { By } from '@angular/platform-browser';
 
 describe('TabComponent', () => {
   let component: TabComponent;
@@ -20,4 +21,20 @@ describe('TabComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+	it("should have .hidden class", () => {
+		const element = fixture.nativeElement.querySelector(".hidden");
+
+		expect(element).toBeTruthy();
+	});
+
+	it("should not have .active class", () => {
+		component.active = true;
+		fixture.detectChanges();
+
+		const element = fixture.nativeElement.querySelector(".active");
+
+		expect(element).not.toBeTruthy();
+	});
+
 });
