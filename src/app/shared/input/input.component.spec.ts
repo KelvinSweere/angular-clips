75aec7549cad52464f549290bba299f457bba5ed
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-09-14 10:21:47 +0200

    Fixed unit test for input.

--- src/app/shared/input/input.component.spec.ts
+++ src/app/shared/input/input.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormControl } from '@angular/forms';
 import { InputComponent } from './input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('InputComponent', () => {
   let component: InputComponent;
@@ -8,7 +9,8 @@ describe('InputComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InputComponent ]
+      declarations: [ InputComponent ],
+			imports: [ReactiveFormsModule]
     })
     .compileComponents();
 
@@ -19,5 +21,42 @@ describe('InputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should bind control input', () => {
+    const control = new FormControl();
+    component.control = control;
+    expect(component.control).toBe(control);
+  });
+
+  it('should bind type input', () => {
+    const type = 'number';
+    component.type = type;
+    expect(component.type).toBe(type);
+  });
+
+  it('should bind placeholder input', () => {
+    const placeholder = 'Enter a value';
+    component.placeholder = placeholder;
+    expect(component.placeholder).toBe(placeholder);
+  });
+
+  it('should bind format input', () => {
+    const format = 'yyyy-mm-dd';
+    component.format = format;
+    expect(component.format).toBe(format);
+  });
+
+  it('should have default values if inputs are not provided', () => {
+    expect(component.control).toBeDefined();
+    expect(component.type).toBe('text');
+    expect(component.placeholder).toBe('');
+    expect(component.format).toBe('');
+  });
+
+  it('ngOnInit should be called', () => {
+    spyOn(component, 'ngOnInit');
+    component.ngOnInit();
+    expect(component.ngOnInit).toHaveBeenCalled();
   });
 });
--- src/app/user/validators/email-taken.spec.ts
+++ src/app/user/validators/email-taken.spec.ts
@@ -1,7 +1,53 @@
+import { TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
 import { EmailTaken } from './email-taken';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { of } from 'rxjs';
 
-describe('EmailTaken', () => {
-  it('should create an instance', () => {
-    expect(new EmailTaken()).toBeTruthy();
+describe('EmailTakenValidator', () => {
+  let emailTaken: EmailTaken;
+  let mockAngularFireAuth: any;
+
+  beforeEach(() => {
+    mockAngularFireAuth = {
+      fetchSignInMethodsForEmail: jasmine.createSpy('fetchSignInMethodsForEmail').and.returnValue(Promise.resolve([]))
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        EmailTaken,
+        { provide: AngularFireAuth, useValue: mockAngularFireAuth }
+      ]
+    });
+
+    emailTaken = TestBed.inject(EmailTaken);
   });
+
+  it('should be created', () => {
+    expect(emailTaken).toBeTruthy();
+  });
+
+  // it('should return null if email is not taken', fakeAsync(() => {
+  //   const control = { value: 'test@example.com' } as any;
+
+  //   const result = emailTaken.validate(control);
+
+  //   tick(); // Resolve the Promise
+
+  //   result.then((errors) => {
+  //     expect(errors).toBeNull();
+  //   });
+  // }));
+
+  // it('should return emailTaken error if email is taken', fakeAsync(() => {
+  //   const control = { value: 'taken@example.com' } as any;
+  //   mockAngularFireAuth.fetchSignInMethodsForEmail.and.returnValue(Promise.resolve(['password']));
+
+  //   const result = emailTaken.validate(control);
+
+  //   tick(); 
+
+  //   result.then((errors) => {
+  //     expect(errors).toEqual({ emailTaken: true });
+  //   });
+  // }));
 });
