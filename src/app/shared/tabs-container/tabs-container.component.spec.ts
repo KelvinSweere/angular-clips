4e6a642832b53e53397812728c664876fa2e3576
Kelvin Sweere <kelvin.sweere@teamrockstars.nl>
2023-09-14 08:13:41 +0200

    Added extra tests for nav component.

--- src/app/shared/tabs-container/tabs-container.component.spec.ts
+++ src/app/shared/tabs-container/tabs-container.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { TabsContainerComponent } from './tabs-container.component';
 import { TabComponent } from '../tab/tab.component';
@@ -37,4 +38,17 @@ describe('TabsContainerComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+	it("should have two tabs", () => {
+		const tabs = fixture.nativeElement.querySelectorAll("app-tab");
+		const containerComponent = fixture.debugElement.query(By.directive(TabsContainerComponent)).componentInstance;
+
+		expect(tabs.length)
+		.withContext("Tabs did not render correctly")
+		.toBe(2); 
+
+		expect(containerComponent.tabs?.length)
+		.withContext("Could not grab component property")
+		.toBe(2);
+	});
 });
